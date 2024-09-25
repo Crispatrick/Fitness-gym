f868bf773c14808f492cab05072a987798ad482c
Fylart <fylartthrowaway0@gmail.com>
2024-09-25 16:13:37 +0900

    First commit, Programs section completed!

---
+++ index.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="css_files\base.css">
+    <link rel="stylesheet" href="css_files\cards.css">
+    <title>JC's Fitness gym</title>
+</head>
+
+<!-- base.css yung mga base design like colors or mga universal design -->
+<!-- sa colors naman may variables na pwede gamitin, nasa base.css din yan. 
+    pag gagamit kayo colors gumamit kayo nun para kung may changes na pagawin si 
+    sir tulad sakin noon, madali nyo nagawa -->
+<!-- naka body = 0 margin nayan -->
+
+<body>
+    <?php
+        include("html_Files\\programs.html");
+    ?>
+</body>
+</html>
