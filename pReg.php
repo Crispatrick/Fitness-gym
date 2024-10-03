06f2fcbbb814950c2a37fa9628613e5b4e9ff934
Fylart <fylartthrowaway0@gmail.com>
2024-10-03 17:27:23 +0900

    update pReg Header

--- pReg.php
+++ pReg.php
@@ -5,13 +5,13 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="css_files\base.css">
     <link rel="stylesheet" href="css_files\pReg.css">
-    <link rel="stylesheet" href="css_files\homepage.css">
+    <link rel="stylesheet" href="css_files\header.css">
     <link rel="stylesheet" href="css_files\footer.css">
     <title>Document</title>
 </head>
 <body>
     <?php
-        include("html_Files\\homepage.html");
+        include("html_Files\\header.html");
         include("html_Files\\preReg.html");
         include("html_Files\\footer.html");
     ?>
