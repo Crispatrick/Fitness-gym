3c01919ddc1bcce0591d79a352b02d5ae60b8096
KID <sandagakristian90@gmail.com>
2024-10-18 10:47:53 +0800

    update

---
+++ rForm.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="css_files\rForm.css">
+    <link rel="stylesheet" href="css_files\header.css">
+    <link rel="stylesheet" href="css_files\footer.css">
+
+    <title>Document</title>
+</head>
+<body>
+    <?php
+        include("html_Files\\header.html");
+        include("html_Files\\regForm.html");
+        include("html_Files\\footer.html");
+    ?>
+</body>
+</html>
