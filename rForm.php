23b60ba50612d65375a8893f1563fc4323b13462
KID <sandagakristian90@gmail.com>
2024-10-21 13:44:06 +0800

    Pulling updates

--- rForm.php
+++ rForm.php
@@ -3,10 +3,11 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="css_files\rForm.css">
+    <link rel="stylesheet" href="css_files\base.css">
     <link rel="stylesheet" href="css_files\header.css">
+    <link rel="stylesheet" href="css_files\rForm.css">
     <link rel="stylesheet" href="css_files\footer.css">
-
+    
     <title>Document</title>
 </head>
 <body>
