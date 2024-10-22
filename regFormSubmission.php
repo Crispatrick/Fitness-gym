dcac9a0f9d59bc3094d319077c470f41f8b6d6e2
KID <sandagakristian90@gmail.com>
2024-10-22 21:36:13 +0800

    regForm DONE!!!!!!!!!

--- regFormSubmission.php
+++ regFormSubmission.php
@@ -4,7 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
-    <link rel="stylesheet" href="../css_Files/rForm.css">
+    <link rel="stylesheet" href="css_Files/rForm.css">
 </head>
 <body>
 
